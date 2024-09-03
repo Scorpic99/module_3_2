ec7e71bc68aa85210090277bcc6cabc4f18aec3f
Александр <116272931+Scorpic99@users.noreply.github.com>
2024-09-03 17:16:40 +0300

    Add files via upload

---
+++ module_3_2.py
@@ -0,0 +1,22 @@
+from re import search
+
+
+def send_email(message, recipient, *, sender = "university.help@gmail.com"):
+    if ((search('.com', recipient) is None and search('.ru', recipient) is None and
+         search('.net', recipient) is None) or (search('.com', sender) is None and
+            search('.ru', sender) is None and search('.net', sender) is None) or
+            (recipient.find('@') < 0 or sender.find('@') < 0)):
+        print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
+    elif recipient == sender:
+        print("Нельзя отправить письмо самому себе!")
+    elif sender == 'university.help@gmail.com':
+        print(f"Письмо успешно отправлено с адреса {sender} на адрес {recipient}.")
+    else:
+        print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.")
+
+
+send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
+send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
+send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
+send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
+
